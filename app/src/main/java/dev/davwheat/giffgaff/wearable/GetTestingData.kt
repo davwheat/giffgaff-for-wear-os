38b2f94b5a309ff1118c52cbab4db2dec51cb40f
David Wheatley <hi@davwheat.dev>
2021-02-02 13:33:55 +0000

    Add new testing data

--- app/src/main/java/dev/davwheat/giffgaff/wearable/GetTestingData.kt
+++ app/src/main/java/dev/davwheat/giffgaff/wearable/GetTestingData.kt
@@ -1,7 +1,8 @@
 package dev.davwheat.giffgaff.wearable
 
 fun GetTestingData(): AccountInfo {
-    val goodybag = Goodybag("BD043", 12, 3, true, 10.00, 9437184, 1048576, 8776581, 1048576)
+    val testTenGolden = Goodybag("BD043", 12, 3, true, 10.00, 9437184, 1048576, 8776581, 1048576, false)
+    val testThirtyGolden = Goodybag("BD046", 12, 3, false, 35.00, -1, 0, -1, -1, true)
 
-    return AccountInfo(420, true, "28/02/2021", goodybag, "07000123456")
+    return AccountInfo(42069, true, "28/02/2021", testThirtyGolden, "07123456789")
 }
