311baca34e714a2693a51bba1389856f74f86d1e
David Wheatley <hi@davwheat.dev>
2021-02-04 01:07:27 +0000

    Comment unused variable

--- app/src/main/java/dev/davwheat/giffgaff/wearable/GetTestingData.kt
+++ app/src/main/java/dev/davwheat/giffgaff/wearable/GetTestingData.kt
@@ -1,7 +1,7 @@
 package dev.davwheat.giffgaff.wearable
 
 fun GetTestingData(): AccountInfo {
-    val testTenGolden = Goodybag("BD043", 12, 3, true, 10.00, 9437184, 1048576, 8776581, 1048576, false)
+//    val testTenGolden = Goodybag("BD043", 12, 3, true, 10.00, 9437184, 1048576, 8776581, 1048576, false)
     val testThirtyGolden = Goodybag("BD046", 12, 3, false, 35.00, -1, 0, -1, -1, true)
 
     return AccountInfo(42069, true, "28/02/2021", testThirtyGolden, "07123456789")
